85d8b2d9711608896f4b15df454b8cc3b5f4ec1e
java0114 <hk109@126.com>
2019-06-26 15:16:40 +0800

    新增java类 1、处理。。。。。

---
+++ AppStartApplication.java
@@ -0,0 +1,11 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStartApplication.class,args);
+    }
+}
